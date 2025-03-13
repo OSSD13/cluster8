b964ac3915c8a75ad74feee4b2bf583458df1730
sirorat88 <sirorat.p88@gmail.com>
2025-03-13 14:09:52 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get("/home",[HomeController::class,'index']);
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'login']);
+
+
+
+

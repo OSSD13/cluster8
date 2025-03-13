3ac7b518907d7ab90b9835651f523846959e849f
Sorayoot Soison <leo.sorayut9@gmail.com>
2025-03-13 15:18:22 +0700

    update

--- app/Http/Middleware/checkLogin.php
+++ app/Http/Middleware/checkLogin.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class checkLogin
+class CheckLogin
 {
     /**
      * Handle an incoming request.
@@ -15,9 +15,8 @@ class checkLogin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $user = session('user');
-        if($user == null || $user->id == null)
-        {
+        $user = session('users');
+        if ($user == null || $user->id == null){
             return redirect('/login');
         }
         return $next($request);

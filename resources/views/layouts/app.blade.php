fde969205b4745efa9b2eb262c57241413d474b6
KittitatNou-Ward <Kittiat2548@gmail.com>
2025-03-13 15:24:52 +0700

    ลบในส่วนเฮดข้างบนออกไป

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">WorkRequest System</a>
-        </div>
-    </nav>
-    
+
     <div class="container mt-4">
         @yield('content')
     </div>

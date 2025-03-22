baa35cae71c616e00b3c6131b623304b6e5e5d55
Setthapong Homchuen <p50520.201@gmail.com>
2025-03-22 13:38:44 +0700

    แก้ชื่อไฟล์ให้ตรงกับเอกสาร

--- app/Http/Controllers/Login_controller.php
+++ app/Http/Controllers/Login_controller.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
 
-class LoginController extends Controller
+class Login_controller extends Controller
 {
     
     function index(){
--- app/Http/Controllers/Work_request_controller.php
+++ app/Http/Controllers/Work_request_controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WorkRequestController extends Controller
+class Work_request_controller extends Controller
 {
     //
     function index(){

6cf5fab71742c6524601d23b163a34c17f30b226
sirorat88 <sirorat.p88@gmail.com>
2025-04-09 16:48:05 +0700

    feat(manage)_09-04-2568
    -เพิ่มฟังก์ชันการกำหนดแผนก
    -เพิ่มฟังก์ชันการลบแผนก
    -เพิ่มฟังก์ชันการ sort by
    -เพิ่มฟังก์ชันการค้นหารายชื่อพนักงาน

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,8 @@ Route::get("/report", [Report_controller::class, "index"]);
 
 Route::get("/manage", [Manage_controller::class, "index"])->middleware([Check_login::class]);
 Route::get('/manage/search-users', [Manage_controller::class, 'searchUsers']);
+Route::post('/manage/search-users', [Manage_controller::class, 'searchUsers'])->name('manage.searchUsers');
 
 Route::post('/manage/edit-dept', [Manage_controller::class, 'edit_dept'])->name('edit.dept');
+
+Route::post('/manage/filter-by-department', [Manage_controller::class, 'filterByDepartment']);
